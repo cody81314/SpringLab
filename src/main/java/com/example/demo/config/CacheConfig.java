785e4fdbd1b3f359c3fb0006e2dd431542a68658
CodyTseng <cody81314@gmail.com>
2018-12-26 10:03:54 +0800

    Add Cache time-to-live

--- src/main/java/com/example/demo/config/CacheConfig.java
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -1,9 +1,46 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableCaching  // 只要宣告 @EnableCaching 啟用了緩存支援，Spring Boot 就會自動配置緩存基礎結構。
 public class CacheConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisCacheManager(
+                RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+                getRedisCacheConfigurationWithTtl(Integer.parseInt(env.getRequiredProperty("spring.cache.redis.time-to-live"))),
+                getRedisCacheConfigurationMap()
+        );
+    }
+
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+
+        redisCacheConfigurationMap.put("userCache", getRedisCacheConfigurationWithTtl(Integer.parseInt(env.getRequiredProperty("cache.user.time-to-live"))));
+
+        return redisCacheConfigurationMap;
+    }
+
+    private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(seconds));
+    }
 }
